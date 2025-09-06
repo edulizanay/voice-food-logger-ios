90766558063395da58a94f34ac50283bbfb9ad21
ealizanay <ealizana@gmail.com>
2025-09-06 08:15:01 +0200

    Initial Commit

---
+++ VoiceFoodLogger/VoiceFoodLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceFoodLoggerApp.swift
+//  VoiceFoodLogger
+//
+//  Created by Eduardo Lizana on 06-09-25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceFoodLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceFoodLoggerTests/VoiceFoodLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceFoodLoggerTests.swift
+//  VoiceFoodLoggerTests
+//
+//  Created by Eduardo Lizana on 06-09-25.
+//
+
+import Testing
+@testable import VoiceFoodLogger
+
+struct VoiceFoodLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
